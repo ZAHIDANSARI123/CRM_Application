8f33a388e25f34fab5fdf13a07b92e30303b2577
ZAHIDANSARI123 <zahidrajagp@gmail.com>
2025-10-12 22:25:59 +0530

    setup the folder and server start

---
+++ index.js
@@ -0,0 +1,10 @@
+const express = require("express")
+const app = express();
+require('dotenv').config();
+
+
+
+const PORT = process.env.PORT || 5555
+app.listen(PORT, ()=>{
+    console.log(`Server started running on the port num : ${process.env.PORT}`)
+})
---
+++ routes/user.model.js
@@ -0,0 +1,38 @@
+const mongoose = require("mongoose")
+
+const userSchema = new mongoose.Schema({
+    name : {
+        type : String,
+        required : true
+    },
+    userId : {
+        type : String,
+        required :true,
+        unique : true
+    },
+    password : {
+        type : String,
+        required : true
+    },
+    email : {
+        type : String,
+        required : true,
+        unique : true,
+        lowercase : true,
+        minLenth : 10
+    },
+    userType : {
+        type : String,
+        enum : ["CUSTOMER", "ADMIN", "ENGINEER"],
+        required : true,
+        default : "CUSTOMER"
+    },
+    userStatus : {
+        type : String,
+        enum : ["APPROVED", "PENDING", "BLOCKED"],
+        required : true,
+        default : "APPROVED"
+    }
+},{timestamps : true});
+
+module.exports = mongoose.model("User", userSchema)
